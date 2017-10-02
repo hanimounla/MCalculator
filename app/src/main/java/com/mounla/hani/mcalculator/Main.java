ee9eddc710901f336466d98718cb691ff77df079
Hani Mounla <hani-mounla@live.com>
2017-10-02 19:44:56 +0300

    Initial commit

---
+++ app/src/main/java/com/mounla/hani/mcalculator/Main.java
@@ -0,0 +1,457 @@
+package com.mounla.hani.mcalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main extends AppCompatActivity implements View.OnLongClickListener
+{
+    public static final String MAIN_ACTIVITY = "MainActivity";
+
+    EditText editText ;
+    String inputExpression;
+    int pos = 0 ;
+    View ans = null , longBackspace = null;
+    public void temp_click(View view)  {
+        TextView t = (TextView) findViewById(R.id.temp);
+        editText.setText(t.getText());
+
+    }
+
+    //region DEFAULTS
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Log.d(MAIN_ACTIVITY, "Create");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        ans = findViewById(R.id.equal);
+        ans.setOnLongClickListener(this);
+        longBackspace = findViewById(R.id.backspace);
+        longBackspace.setOnLongClickListener(longBackspace(savedInstanceState));
+        editText.setSelection(pos);
+
+        try {
+            Button about = (Button)findViewById(R.id.aboutBTN);
+            about.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getApplicationContext(),About.class));
+                }
+            });
+        } catch (Exception e) {
+
+        }
+    }
+    public View.OnLongClickListener longBackspace(Bundle v)
+    {
+        editText.setText("");
+        return null;
+    }
+    @Override
+    public boolean onLongClick(View v)
+    {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("ans");
+        return true;
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();  // Always call the superclass
+        Log.d(MAIN_ACTIVITY,"Destroy");
+        android.os.Debug.stopMethodTracing();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle presses on the action bar items
+        switch (item.getItemId())
+        {
+            case R.id.about : startActivity(new Intent(this, About.class));
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(MAIN_ACTIVITY, "Pause");
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(MAIN_ACTIVITY, "Resume");
+    }
+    //endregion
+
+    //region NUMBERPANEL
+    public void n0(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("0");
+    }
+
+    public void n1(View view){
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("1");
+    }
+
+    public void n2(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("2");
+    }
+
+    public void n3(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("3");
+    }
+
+    public void n4(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("4");
+    }
+
+    public void n5(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("5");
+    }
+
+    public void n6(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("6");
+    }
+
+    public void n7(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("7");
+    }
+
+    public void n8(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("8");
+
+    }
+
+    public void n9(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("9");
+    }
+
+    public void dot(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition(".");
+    }
+    //endregion
+
+    //region OPERATIONS
+    public void plus(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("+");
+    }
+
+    public void mines(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("-");
+    }
+
+    public void mul(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("×");
+    }
+
+    public void div(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("÷");
+    }
+    //endregion
+
+    private void correctPosition(String s) {
+        pos = editText.getSelectionStart();
+        char [] insert = inputExpression.toCharArray();
+        String toEnd = "" ,toPos = "";
+        if(pos != 0)
+            for (int i = 0; i < pos; i++)
+                toPos += insert[i];
+        for (int i = pos ; i <insert.length ; i++)
+            toEnd += insert[i];
+        toPos += s;
+        pos += s.length();
+        editText.setText(toPos + toEnd);
+        editText.setSelection(pos);
+    }
+
+    //region SCIENTIFIC
+    public void sin(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("sin(");
+    }
+
+    public void cos(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("cos(");
+    }
+
+    public void tan(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("tan(");
+    }
+
+    public void log(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("log(");
+    }
+
+
+    public void ln(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        char [] insert = inputExpression.toCharArray();
+        String toEnd = "" ,toPos = "";
+        if(pos != 0)
+            for (int i = 0; i < pos; i++)
+                toPos += insert[i];
+        for (int i = pos ; i <insert.length ; i++)
+            toEnd += insert[i];
+        toPos += "ln(";
+        pos += 3;
+        editText.setText(toPos + toEnd);
+        editText.setSelection(pos);
+    }
+
+    public void sqrt(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        char [] insert = inputExpression.toCharArray();
+        String toEnd = "" ,toPos = "";
+        if(pos != 0)
+            for (int i = 0; i < pos; i++)
+                toPos += insert[i];
+        for (int i = pos ; i <insert.length ; i++)
+            toEnd += insert[i];
+        toPos += "√(";
+        pos += 2;
+        editText.setText(toPos + toEnd);
+        editText.setSelection(pos);
+    }
+
+    public void pow(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        char [] insert = inputExpression.toCharArray();
+        String toEnd = "" ,toPos = "";
+        if(pos != 0)
+            for (int i = 0; i < pos; i++)
+                toPos += insert[i];
+        for (int i = pos ; i <insert.length ; i++)
+            toEnd += insert[i];
+        toPos += "^(";
+        pos += 2;
+        editText.setText(toPos + toEnd);
+        editText.setSelection(pos);
+    }
+
+    public void fact(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        char [] insert = inputExpression.toCharArray();
+        String toPos = "" , toEnd = "";
+        if(pos != 0)
+            for (int i = 0; i < pos ; i++)
+                toPos += insert[i];
+        for (int i = pos ; i <insert.length ; i++)
+            toEnd +=insert[i];
+        toPos += "!";
+        pos++;
+        editText.setText(toPos + toEnd);
+        editText.setSelection(pos);
+    }
+
+    public void e(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("e");
+    }
+
+    public void percent(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        correctPosition("%");
+    }
+    //endregion
+
+    //region MAIN BUTTONS
+    public void left(View view) {
+        if(pos != 0)
+        {
+            pos = editText.getSelectionStart();
+            pos--;
+            editText.setSelection(pos);
+        }
+    }
+
+    public void right(View view) {
+        int lenght = editText.length();
+        if(editText != null && pos < lenght )
+        {
+            pos = editText.getSelectionStart();
+            pos++;
+            editText.setSelection(pos);
+        }
+    }
+
+    public void allClear(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        editText.setText("");
+        TextView t = (TextView) findViewById(R.id.temp);
+        t.setText("");
+        pos = 0;
+        //lynda();
+    }
+
+    public void brackets(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        char [] equation = inputExpression.toCharArray();
+        int explenght = equation.length;
+        char correctbrackert = '(';
+        int openbrkt = 0, closebrkt = 0, i = 0;
+        while (i < explenght)
+        {
+            if (equation[i] == '(')
+                openbrkt = i;
+            if (equation[i] == ')')
+                closebrkt = i;
+            i++;
+        }
+        if (openbrkt > closebrkt)
+            correctbrackert = ')';
+        String toEnd = "" ,toPos = "";
+        if(pos != 0)
+            for (int j = 0; j < pos; j++)
+                toPos += equation[j];
+        for (int j = pos ; j <equation.length ; j++)
+            toEnd += equation[j];
+        toPos += correctbrackert;
+        pos++;
+        editText.setText(toPos + toEnd);
+        editText.setSelection(pos);
+    }
+
+    public void backspace(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        inputExpression = editText.getText().toString();
+        char [] insert = inputExpression.toCharArray();
+        if(pos != 0)
+        {
+            String toPos = "", toEnd = "";
+            for (int i = 0; i < pos; i++)
+                toPos += insert[i];
+            for (int i = pos; i < insert.length; i++)
+                toEnd += insert[i];
+            char delete[] = toPos.toCharArray();
+            toPos = "";
+            for (int i = 0; i < pos-1; i++)
+                toPos += delete[i];
+            pos--;
+            editText.setText(toPos + toEnd);
+            editText.setSelection(pos);
+        }
+    }
+
+    public void equal(View view) {
+        try
+        {
+            TextView temp = (TextView) findViewById(R.id.temp);
+            editText = (EditText) findViewById(R.id.editText);
+            inputExpression = editText.getText().toString();
+            MathHelper m = new MathHelper();
+            double num = m.evaluate(inputExpression);
+            temp.setText(inputExpression + " =");
+            String done;
+            done = num + "";
+            if (done.contains(".0") && done.lastIndexOf("0") == done.length() - 1)
+                done =(long)num + "";
+            editText.setText(done + "");
+            pos = editText.length();
+            editText.setSelection(pos);
+        }
+        catch (Exception e)
+        {
+            Toast t = Toast.makeText(this,"Syntax Error !", Toast.LENGTH_SHORT );
+            t.show();
+        } catch (Throwable throwable) {
+            Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void plot(View view) {
+        startActivity(new Intent(getApplication(),PlottingFunctions.class));
+    }
+
+    //endregion
+
+
+
+
+    //region Lynda
+    /*public void lynda()
+    {
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify = new Notification(android.R.drawable.stat_notify_more,
+                "Clear Button was pressed !",System.currentTimeMillis());
+        Context context = MainActivity.this;
+        CharSequence title = "You Have Been Notified";
+        CharSequence details = "Continue what you where doing";
+        Intent intent = new Intent(context,MainActivity.class);
+        PendingIntent pending = PendingIntent.getActivity(context,0,intent,0);
+        notify.setLatestEventInfo(context, title, details, pending);
+        notify.sound = Uri.parse("android.resource://com.mounla.hani.mcalculator/" + R.raw.beep);
+        nm.notify(0, notify);
+    }*/
+
+
+    //endregion
+}
---
+++ app/src/main/java/com/mounla/hani/mcalculator/MathHelper.java
@@ -0,0 +1,233 @@
+package com.mounla.hani.mcalculator;
+
+import java.util.Stack;
+
+/**
+ * Created by hani-_000 on 2017-10-02.
+ */
+
+public class MathHelper {
+
+    //region EVALUATION
+
+    Stack<Double> values = new Stack<>();
+    Stack<Character> ops = new Stack<>();
+    double answer ;
+
+
+    public  double evaluate(String input) throws Throwable {
+
+        char [] tokens = input.toCharArray();
+        for (int i = 0; i < tokens.length; i++)
+        {
+            if (isletter(tokens[i]))
+            {
+                String word = "" ;
+                while (i < tokens.length && isletter(tokens[i]))
+                    word += tokens[i++];
+                if (word.contains("ans")) {
+                    values.push(answer);
+                    i--;
+                }
+                else
+                {
+                    String sub = "";
+                    double betweenbrackts;
+                    i++;
+                    while (tokens[i] != ')')
+                    {
+                        sub += tokens[i];
+                        i++;
+                    }
+                    betweenbrackts = evaluate(sub);
+                    if (word.lastIndexOf("^") == 0)
+                        values.push(doMath(word, betweenbrackts, values.pop()));
+                    else
+                        values.push(doMath(word, betweenbrackts, 0.0));
+                }
+            }
+            else if (isNumber(tokens[i]))
+            {
+                StringBuilder digitsNumber = new StringBuilder();
+
+                if(tokens[i] == 'e')
+                    values.push(Math.E);
+                else
+                {
+                    if (tokens[0] == '-' && i == 1)
+                        digitsNumber.append(tokens[0]);
+                    while ((i < tokens.length && isNumber(tokens[i]))) // if the number is more than on digit
+                        digitsNumber.append(tokens[i++]);
+                    values.push(Double.parseDouble(digitsNumber.toString()));
+                    i--;
+                }
+            }
+            else if (tokens[i] == '%') // if its a percent character
+                values.push(values.pop()/100);
+            else if (tokens[i] == '!') // if its a Factorial character
+                values.push(factorial(values.pop()));
+
+            else if (isOp(tokens[i])) // if its a operation sign
+            {
+                if(tokens[i] =='-' && i == 0)
+                    continue;
+                while (!ops.empty() && hasPriority(tokens[i], ops.peek()))
+                    values.push(doOperation(ops.pop(), values.pop(), values.pop()));
+                ops.push(tokens[i]);
+            }
+            else if (tokens[i] == '(') // if its an open bracket
+                ops.push(tokens[i]);
+
+            else if (tokens[i] == ')') // if its a close bracket
+            {                          // evaluate the string until we see the '('
+                while (ops.peek() != '(')
+                    values.push(doOperation(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+            }
+        }
+        while (!ops.isEmpty()) // do all the operations until the operation stack is empty
+            values.push(doOperation(ops.pop(), values.pop(), values.pop()));
+        answer = values.pop();
+        return answer;
+    }
+
+//    private double subEvaluate(String sub) {
+//        char [] tokens = sub.toCharArray();
+//        Stack<Double> values = new Stack<>();
+//        Stack<Character> ops = new Stack<>();
+//        double subAnswer;
+//        for (int i = 0; i < tokens.length; i++)
+//        {
+//            if (isletter(tokens[i]))
+//            {
+//                StringBuilder word = new StringBuilder();
+//                while (i < tokens.length && isletter(tokens[i]))
+//                    word.append(tokens[i++]);
+//                String subsub ="";
+//                double betweenbrackts;
+//                i++;
+//                while (tokens[i] != ')')
+//                {
+//                    subsub +=tokens[i];
+//                    i++;
+//                }
+//                betweenbrackts = Double.parseDouble(sub);
+//                values.push(doMath(word.toString() , betweenbrackts, values.peek()));
+//            }
+//            else if (isNumber(tokens[i]))
+//            {
+//                StringBuilder digitsNumber = new StringBuilder();
+//
+//                if(tokens[i] == 'e')
+//                    values.push(Math.E);
+//                else
+//                {
+//                    if (tokens[0] == '-' && i == 1)
+//                        digitsNumber.append(tokens[0]);
+//                    while ((i < tokens.length && isNumber(tokens[i]))) // if the number is more than on digit
+//                        digitsNumber.append(tokens[i++]);
+//                    values.push(Double.parseDouble(digitsNumber.toString()));
+//                    i--;
+//                }
+//            }
+//            else if (isOp(tokens[i])) // if its a operation sign
+//            {
+//                if(tokens[i] =='-' && i == 0)
+//                    continue;
+//                while (!ops.empty() && hasPriority(tokens[i], ops.peek()))
+//                    values.push(doOperation(ops.pop(), values.pop(), values.pop()));
+//                ops.push(tokens[i]);
+//            }
+//            else if (tokens[i] == '(') // if its an open bracket
+//                ops.push(tokens[i]);
+//
+//            else if (tokens[i] == ')') // if its a close bracket
+//            {                          // evaluate the string until we see the '('
+//                while (ops.peek() != '(')
+//                    values.push(doOperation(ops.pop(), values.pop(), values.pop()));
+//                ops.pop();
+//            }
+//        }
+//        while (!ops.isEmpty()) // do all the operations until the operation stack is empty
+//            values.push(doOperation(ops.pop(), values.pop(), values.pop()));
+//        subAnswer = values.pop();
+//        return subAnswer;
+//    }
+
+    private double doMath(String word, double subNumber, double num) {
+        switch (word)
+        {
+            case "sin" : return Math.sin(subNumber*Math.PI/180);
+            case "cos" : return Math.cos(subNumber * Math.PI / 180);
+            case "tan" : return Math.tan(subNumber * Math.PI / 180);
+            case "ln" : return Math.log(subNumber);
+            case "log" : return Math.log10(subNumber);
+            case "√" : return Math.sqrt(subNumber);
+            case "^" : return Math.pow(num,subNumber);
+        }
+        return 0;
+    }
+
+    public boolean isNumber(char c) {
+        return (c >= '0' && c <= '9')
+                || c == '.' || c == 'e' || c == 'E';
+    }
+
+    public boolean isOp(char c) {
+        return c == '+' || c == '-' ||
+                c == '×' || c == '÷' || c == '%' || c== '!';
+    }
+
+    public boolean hasPriority(char op1, char op2) // check the priority between the current char (op1)
+    {                                              // and the operation stack top (op2) if current char
+        if(op2 =='(' || op2 == ')')          // is more important than the operation stack top do its operation
+            return false;
+        else if((op1 == '×' || op1 == '÷') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    private  double doOperation(char op, double b, double a) throws Throwable {
+        switch(op)
+        {
+            case '+' : return a + b;
+            case '-' : return a - b;
+            case '×' : return a * b;
+            case '*' : return a * b;
+            case '%' : return b /100;
+            case '/' :
+                if(b == 0) {
+                    throw new Throwable("\"Cannot divide by Zero ! \\n check your input\"");
+//                    Toast t = Toast.makeText(getApplicationContext(),, 2000 );
+//                    t.show();
+                }
+                else
+                    return a / b;
+            case '÷' :
+                if(b == 0) {
+                    throw new Throwable("\"Cannot divide by Zero ! \\n check your input\"");
+
+//                    Toast t = Toast.makeText(this,"Cannot divide by Zero ! \n check your input", 2000 );
+//                    t.show();
+                }
+                else
+                    return a / b;
+        }
+        return 0;
+    }
+
+    private double factorial(double num) {
+        double fact = 1;
+        if(num >1)
+            fact = num *factorial(num-1);
+        return fact;
+    }
+
+    public boolean isletter(char c) {
+        return c >= 'a' && c <= 'z' && c != 'e' || c == '√' || c == '^';  // Simplified if statment it will return
+        // true if any of these conditions are true.
+    }
+    //endregion
+
+}
